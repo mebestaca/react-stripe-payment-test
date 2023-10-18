a1a2e3cb6c0aa373e3a43ba03b5b8986ae591d4b
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-17 23:15:55 -0600

    modified app.js to include paymentform and show the test store page

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import StripeContainer from './components/StripeContainer';
+import { useState } from 'react';
+
 
 function App() {
+  const [showItem, setShowItem] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test Store</h1>
+      {
+        showItem ? 
+        <StripeContainer/> :
+        <>
+            <h3>$10.00</h3>
+            <img src="https://product-images.metro.ca/images/h1b/h78/9531757527070.jpg" alt="plunger" />
+            <button onClick={ ()=> setShowItem(true) }>Purchase</button>
+        </>
+      }
     </div>
   );
 }
